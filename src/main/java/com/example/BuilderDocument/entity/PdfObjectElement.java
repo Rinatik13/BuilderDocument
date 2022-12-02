637e6f6c58ebb2444d1a03b698b98a38089f9995
Rinat <61909431+Rinatik13@users.noreply.github.com>
2022-12-02 15:21:35 +0500

    создал POJO

--- src/main/java/com/example/BuilderDocument/entity/PdfObjectElement.java
+++ src/main/java/com/example/BuilderDocument/entity/PdfObjectElement.java
@@ -2,9 +2,31 @@ package com.example.BuilderDocument.entity;
 
 public class PdfObjectElement implements PdfElement {
 
+    private String type;
+    private String function;
+
     @Override
     public String getElement() {
+
         return null;
     }
 
+    public PdfObjectElement() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
 }
